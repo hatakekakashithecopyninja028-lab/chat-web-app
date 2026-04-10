01c7064d38bd939817273574be4574fd1883d147
sumit kumar <hatakekakashithecopyninja028@gmail.com>
2026-04-10 12:21:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,10 @@ app.use("/api/messages",messagerouter)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+ 
+  app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
